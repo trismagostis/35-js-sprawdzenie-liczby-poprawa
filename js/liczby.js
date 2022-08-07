44bc2b9e2b491add937092e756785eb7d66f6d60
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-08-07 19:31:14 +0200

    poprawa

---
+++ js/liczby.js
@@ -0,0 +1,7 @@
+function przedzial(number) {
+    if (number<200 && number>100) {
+      console.log("Liczba znajduje się w przedziale");
+    } 
+      else  {console.log("Liczba nie znajduje się w przedziale");}
+  }
+  przedzial(150);
